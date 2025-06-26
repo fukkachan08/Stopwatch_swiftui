4dc89f8b4dc8f152a34c33fd0d92c5e31e2549d7
Fukano Masato <marbo0805@gmail.com>
2025-06-26 18:48:14 +0900

    Initial Commit

---
+++ Stopwatch_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stopwatch_swiftui
+//
+//  Created by 深野真人 on 2025/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stopwatch_swiftui/Stopwatch_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stopwatch_swiftuiApp.swift
+//  Stopwatch_swiftui
+//
+//  Created by 深野真人 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Stopwatch_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
